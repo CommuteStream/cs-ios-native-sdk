4a031f3ca561d826eafb81cf77a329b53811cc52
Tom Burdick <thomas.burdick@gmail.com>
2017-03-16 16:07:13 -0500

    feat: universal build, configurable mock client
    Adds a universal build and a mock client that ads may be added to

--- CSNative/CSNMockClient.h
+++ CSNative/CSNMockClient.h
@@ -1,6 +1,10 @@
 @import Foundation;
 #import "CSNClient.h"
+#import "CSNAd.h"
+#import "CSNStopTuple.h"
 
 @interface CSNMockClient : NSObject <CSNClient>
+- (instancetype) init;
 - (void) getAds:(CSNPAdRequest *)adRequest success:(void (^)(CSNPAdResponse *))success failure:(void (^)(NSError *))failure;
+- (void) addStopAd:(CSNPNativeAd *)ad stop:(CSNPStop *)stop;
 @end
--- CSNative/CSNMockClient.m
+++ CSNative/CSNMockClient.m
@@ -1,12 +1,15 @@
 #import "CSNMockClient.h"
 
+@interface CSNMockClient ()
+@property NSMutableDictionary *ads;
+@property NSMutableDictionary *stopAds;
+@end
+
 @implementation CSNMockClient
-- (void) getAds:(CSNPAdRequest *)adRequest success:(void (^)(CSNPAdResponse *))success failure:(void (^)(NSError *))failure
-{
-    CSNPAdResponse *response = [[CSNPAdResponse alloc] init];
-    CSNPNativeAd *testAd = [[CSNPNativeAd alloc] init];
-    [testAd setAdId:0];
-    [testAd setRequestId:0];
+
+- (instancetype) init {
+    _ads = [[NSMutableDictionary alloc] init];
+    _stopAds = [[NSMutableDictionary alloc] init];
     CSNPTitleComponent *title = [[CSNPTitleComponent alloc] init];
     [title setComponentId:0];
     [title setTitle:@"Test Ad Title"];
@@ -15,16 +18,39 @@
     NSString *imagePath = [[NSBundle bundleForClass:[CSNMockClient class]] pathForResource:@"cs" ofType:@"png"];
     NSData *imageData = [NSData dataWithContentsOfFile:imagePath];
     [icon setImage:imageData];
-    [testAd setTitle:title];
+    CSNPNativeAd *testAd = [[CSNPNativeAd alloc] init];
+    [testAd setAdId:99999999];
+    [testAd setRequestId:0];
     [testAd setIcon:icon];
-    [[response ads] setObject:testAd forKey:[testAd adId]];
+    [testAd setTitle:title];
+    CSNPStop *stop = [[CSNPStop alloc] init];
+    [stop setAgencyId:@"commutestream"];
+    [stop setRouteId:@"test"];
+    [stop setStopId:@"test"];
+    [self addStopAd:testAd stop:stop];
+
+    return self;
+}
+
+- (void) getAds:(CSNPAdRequest *)adRequest success:(void (^)(CSNPAdResponse *))success failure:(void (^)(NSError *))failure
+{
+    CSNPAdResponse *response = [[CSNPAdResponse alloc] init];
     for(id stop in [adRequest stopsArray]) {
+        NSNumber *adId = [_stopAds objectForKey:stop];
+        CSNPNativeAd *ad = [_ads objectForKey:adId];
         CSNPStopAd *stopAd = [[CSNPStopAd alloc] init];
         [stopAd setStopTuple:stop];
-        [stopAd setAdId:[testAd adId]];
+        [stopAd setAdId:[ad adId]];
         [[response stopAdsArray] addObject:stopAd];
+        [[response ads] setObject:ad forKey:[ad adId]];
     }
     success(response);
+}
+
+- (void) addStopAd:(CSNPNativeAd *)ad stop:(CSNPStop *)stop {
+    NSNumber *adID = [NSNumber numberWithUnsignedLongLong:[ad adId]];
+    [_stopAds setObject:adID forKey:stop];
+    [_ads setObject:ad forKey:adID];
 }
 
 - (void) sendAdReport:(CSNPAdReport *)adReport success:(void (^)())success failure:(void (^)(NSError *))failure {
