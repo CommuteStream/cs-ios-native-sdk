a94dcf0a128e721fef95125edbdf39e14e77b484
Tom Burdick <thomas.burdick@gmail.com>
2017-09-21 12:56:11 -0500

    fix navigation for links

--- CSNative/CSNHeroView.m
+++ CSNative/CSNHeroView.m
@@ -21,6 +21,7 @@
         [heroWebView loadHTMLString:[[ad hero] html] baseURL:nil];
         [heroWebView setFrame: self.bounds];
         [heroWebView setUserInteractionEnabled:[[ad hero] interactive]];
+        [heroWebView setNavigationDelegate:self];
         [self addSubview:heroWebView];
     } else {
         UIImageView *heroImageView = [[UIImageView alloc] initWithImage:[[ad hero] image]];
@@ -28,6 +29,22 @@
         [self addSubview:heroImageView];
     }
     [self setNeedsDisplay];
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(nonnull WKNavigationAction *)navigationAction decisionHandler:(nonnull void (^)(WKNavigationActionPolicy))decisionHandler
+{
+    if (navigationAction.navigationType == WKNavigationTypeLinkActivated) {
+        if (navigationAction.request.URL) {
+            NSLog(@"%@", navigationAction.request.URL.host);
+      
+            if ([[UIApplication sharedApplication] canOpenURL:navigationAction.request.URL]) {
+                [[UIApplication sharedApplication] openURL:navigationAction.request.URL];
+                decisionHandler(WKNavigationActionPolicyCancel);
+            }
+        }
+    } else {
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
 }
                                                                                                                
 @end
